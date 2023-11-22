7fdf5eb66b68931280c2d32abb40b4e50508e61a
Catherine Komi <oceanbeaats@gmail.com>
2023-11-21 20:41:43 -0700

    add DynamoDB logic + update Lambda

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,4 +1,17 @@
+/* eslint-disable react/prop-types */
 import PropTypes from "prop-types";
+
+const IngredientDetails = ({ ingredient }) => (
+  <div>
+    <p className='mt-2 text-slate-500'>Name: {ingredient.name}</p>
+    <p className='mt-2 text-slate-500'>
+      Ingredient ID: {ingredient.ingredientId}
+    </p>
+    <p className='mt-2 text-slate-500'>Unit: {ingredient.unit}</p>
+    <p className='mt-2 text-slate-500'>Quantity: {ingredient.quantity}</p>
+    <p className='mt-2 text-slate-500'>Notes: {ingredient.notes}</p>
+  </div>
+);
 
 const RecipeCard = (props) => {
   const recipes = props.recipes;
@@ -6,7 +19,7 @@ const RecipeCard = (props) => {
   return (
     <div className='max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl'>
       {recipes.map((recipe) => (
-        <div className='md:flex' key={recipe.customerId}>
+        <div className='md:flex' key={recipe.recipeId}>
           <div className='md:shrink-0'>
             <img
               className='h-48 w-full object-cover md:h-full md:w-48'
@@ -24,7 +37,10 @@ const RecipeCard = (props) => {
             >
               {recipe.recipeName}
             </a>
-            <p className='mt-2 text-slate-500'>{recipe.ingredients}</p>
+            {Array.isArray(recipe.ingredients) &&
+              recipe.ingredients.map((ingredient, index) => (
+                <IngredientDetails key={index} ingredient={ingredient} />
+              ))}
           </div>
         </div>
       ))}
@@ -35,11 +51,10 @@ const RecipeCard = (props) => {
 RecipeCard.propTypes = {
   recipes: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired, // Adjust the type according to your data structure
-      recipeName: PropTypes.string.isRequired,
-      // Add more PropTypes for other properties as needed
+      id: PropTypes.number,
+      recipeName: PropTypes.string,
     })
-  ).isRequired,
+  ),
 };
 
 export default RecipeCard;
